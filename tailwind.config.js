430ba02dbcb607560b8977287f1162e40933e4d3
Sergio <sergio.pm.developer@gmail.com>
2024-02-12 23:33:00 +0000

    style: add theme colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-1': '#2B2D42',
+        'theme-2': '#8D99AE',
+        'theme-3': '#EDF2F4',
+        'theme-4': '#EF233C',
+      },
+      fontFamily: {
+        'theme-1': ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
