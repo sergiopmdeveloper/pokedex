4c48f32830da293d8038b87a6e48fd13f509921b
Sergio <sergio.pm.developer@gmail.com>
2024-02-13 02:16:40 +0000

    docs: add Pokemon component documentation

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -2,6 +2,12 @@ import type { Pokemon } from '../types/pokemon'
 
 type PokemonProps = Pokemon
 
+/**
+ * Renders a Pokemon component.
+ * @param {PokemonProps} props - The component props.
+ * @param {string} props.name - The name of the Pokemon.
+ * @returns The rendered Pokemon component.
+ */
 export default function Pokemon({ name }: PokemonProps) {
   return (
     <div>
